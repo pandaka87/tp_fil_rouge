9458ef2db1ab6eee4e430d759e113749fa112c17
Noellie P <noellie.peuch@gmail.com>
2023-07-31 09:33:40 +0200

    Initialize project

---
+++ src/main/java/fr/groupe_3/fil_rouge/FilRougeApplication.java
@@ -0,0 +1,13 @@
+package fr.groupe_3.fil_rouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilRougeApplication.class, args);
+	}
+
+}
