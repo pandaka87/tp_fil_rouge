0bbe3b6776651e6454f0ba3af96a452d8a84ea68
Ebru Jolivet <ebruozogul1@gmail.com>
2023-07-31 11:58:38 +0200

    parti_client_terminée

---
+++ src/main/java/fr/groupe_3/fil_rouge/entity/Client.java
@@ -0,0 +1,161 @@
+package fr.groupe_3.fil_rouge.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+  // Attributs
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "company_name")
+  private String companyName;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  private String email;
+
+  private String phone;
+
+  private String adress;
+
+  @Column(name = "zip_code")
+  private String zipCode;
+
+  private String city;
+
+  private String country;
+
+  // @Column(name = "state")
+  // @Enumerated(EnumType.STRING)
+  // private EtatClient state;
+
+  private Integer state;
+
+  // Constructeurs
+  public Client() {
+
+  }
+
+  public Client(String companyName, String firstName, String lastName, String email, String phone,
+      String adress, String zipCode, String city, String country, Integer state) {
+    this.companyName = companyName;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.adress = adress;
+    this.zipCode = zipCode;
+    this.city = city;
+    this.country = country;
+    this.state = state;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getAdress() {
+    return adress;
+  }
+
+  public void setAdress(String adress) {
+    this.adress = adress;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  // public EtatClient getState() {
+  // return state;
+  // }
+
+  // public void setState(EtatClient state) {
+  // this.state = state;
+  // }
+
+  public Integer getState() {
+    return state;
+  }
+
+  public void setState(Integer state) {
+    this.state = state;
+  }
+}
